c95cc88682931a52e8ac8da54c4fcea89330757f
bilzard <nakamine11@gmail.com>
2026-04-10 21:10:51 +0900

    G/Hキーの定義を削除

--- keyboards/salicylic_acid3/7skb/keymaps/svmv2/keymap.c
+++ keyboards/salicylic_acid3/7skb/keymaps/svmv2/keymap.c
@@ -116,8 +116,6 @@ svm_config_t get_svm_params(uint16_t tap_kc) {
         case KC_SPC: case KC_BSPC: case KC_ENT:
         case KC_SEMICOLON: case KC_EQUAL: case KC_MINUS:
             return (svm_config_t){.w_x=1000, .w_y=-200, .b=-127000, .guard=250};
-        case KC_G: case KC_H:
-            return (svm_config_t){.w_x=1000, .w_y=0, .b=-180000, .guard=180};
         case KC_A: case KC_S: case KC_D: case KC_F:
         case KC_J: case KC_K: case KC_L:
             return (svm_config_t){.w_x=1000, .w_y=0, .b=-250000, .guard=250};
