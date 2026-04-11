3134945a8204a5251d44c5b0ae10e75d9e25068f
bilzard <nakamine11@gmail.com>
2026-04-11 19:25:30 +0900

    change SVM parameter

--- keyboards/salicylic_acid3/7skb/keymaps/svmv2/keymap.c
+++ keyboards/salicylic_acid3/7skb/keymaps/svmv2/keymap.c
@@ -116,7 +116,7 @@ svm_config_t get_svm_params(uint16_t tap_kc) {
     switch (tap_kc) {
         case KC_SPC: case KC_BSPC: case KC_ENT:
         case KC_SEMICOLON: case KC_EQUAL: case KC_MINUS:
-            return (svm_config_t){.w_x=1000, .w_y=-453, .b=-77251, .guard=250};
+            return (svm_config_t){.w_x=1000, .w_y=-296, .b=-88222, .guard=250};
         case KC_A: case KC_S: case KC_D: case KC_F:
         case KC_J: case KC_K: case KC_L:
             return (svm_config_t){.w_x=1000, .w_y=0, .b=-250000, .guard=250};
